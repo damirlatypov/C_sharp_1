f493367656907f3e0c40459896c04dd811b1c557
damirlatypov <damir251994@gmail.com>
2023-07-02 22:50:43 +0300

    Сделал классную и домашнюю работу

---
+++ Lesson_1/1_DZ_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int a;                             
+Console.Write("Введите число: ");
+a= Console.Read();
+if (a % 2==1)
+{
+Console.WriteLine("Число нечетное");
+}
+else
+{
+Console.WriteLine("Число четное");
+}
+ 
