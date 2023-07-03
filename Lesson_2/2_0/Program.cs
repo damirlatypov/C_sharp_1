3f4aa848224218a66cd7501faa13875c4467136a
damirlatypov <damir251994@gmail.com>
2023-07-03 21:49:22 +0300

    ClassWork 2_0

---
+++ Lesson_2/2_0/Program.cs
@@ -0,0 +1,15 @@
+﻿int TakeNum(int num)
+	{
+	    Console.WriteLine(num);
+	    return num % 10;
+	}
+	
+	// 123 % 10 -> 3
+	// 123 % 100 -> 23
+	
+	// 123 / 10 -> 12
+	// 123 / 100 -> 1
+	
+	int th_num = new Random().Next(100, 1000);
+	int result = TakeNum(th_num);
+	Console.WriteLine(result);
